9afbd1cb9f33ef0b6418612698b5551e98a8587e
rgoles <renato.goles@gmail.com>
2023-05-07 12:11:25 +0200

    RockPaperScissors

--- js/game.js
+++ js/game.js
@@ -16,9 +16,7 @@ function playRound(playerSelection, computerSelection) {
     }
 
     // Check for all other cases
-    if ((player === 'rock' && computer === 'scissors') ||
-        (player === 'paper' && computer === 'rock') ||
-        (player === 'scissors' && computer === 'paper')) {
+    if ((player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player === 'scissors' && computer === 'paper')) {
         return `You win! ${player} beats ${computer}.`;
     } else {
         return `You lose! ${computer} beats ${player}.`;
@@ -26,18 +24,42 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        // Get the player and computer selections
-        let playerSelection = prompt("Enter rock, paper, or scissors:");
-        let computerSelection = getComputerChoice();
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         // Get the player and computer selections
+//         let playerSelection = prompt("Enter rock, paper, or scissors:");
+//         let computerSelection = getComputerChoice();
+//
+//         // Play the round and output the result
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
 
-        // Play the round and output the result
-        console.log(playRound(playerSelection, computerSelection));
-    }
+const rockButton = document.getElementById('rock-button')
+const paperButton = document.getElementById('paper-button')
+const scissorsButton = document.getElementById('scissors-button')
+
+rockButton.addEventListener('click', function () {
+    playGame('rock')
+})
+
+paperButton.addEventListener('click', function () {
+    playGame('paper')
+})
+
+scissorsButton.addEventListener('click', function () {
+    playGame('scissors')
+})
+
+// Define the playGame function
+function playGame (playerSelection) {
+    // Get the computer's selection
+    const computerSelection = getComputerChoice()
+
+    // Play the round and output the result
+    const result = playRound(playerSelection, computerSelection)
+    // console.log(result
+        alert(result)
 }
 
-let playerSelection = prompt("Enter rock, paper, or scissors:");
-let computerSelection = getComputerChoice()
-
-game()
+playGame()
